7bcefb52d11592cfdc78bffbe9f51166fe9bbac0
Cameron <cameron.ottens@gmail.com>
2022-03-18 15:17:15 -0700

    added last edits to chaining.js

---
+++ chaining.js
@@ -0,0 +1,16 @@
+let num = 1;
+let firstName = 'Aaron';
+
+let numAndName = num === 0 || firstName === 'Aaron'
+
+if (numAndName){
+    console.log('this was true');
+} else {
+    console.log('this was false')
+}
+
+let dems = false;
+let reps = false;
+
+let either = dems || reps
+console.log(either)
